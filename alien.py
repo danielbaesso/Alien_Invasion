b17aa1c31cb5d6395badeb7f3264c2ca40b44046
Daniel Baesso <danielbaesso@gmail.com>
2021-04-26 17:51:07 -0300

    Corrections

--- alien.py
+++ alien.py
@@ -2,31 +2,27 @@ import pygame
 from pygame.sprite import Sprite
 
 class Alien(Sprite):
-    """Uma classe que representa um único alienígena da frota."""
+    """A class to represent a single alien in the fleet."""
 
     def __init__(self, ai_settings, screen):
-        """Inicializa o alien e define sua posição"""
+        """Initialize the alien, and set its starting position."""
         super(Alien, self).__init__()
         self.screen = screen
         self.ai_settings = ai_settings
 
-        # Carrega a imagem do alienígena e define seu atributo rect
+        # Load the alien image, and set its rect attribute.
         self.image = pygame.image.load("./figures/alien.bmp")
         self.rect = self.image.get_rect()
 
-        # Inicia cada novo alienígena próximo à parte superior esquerda da tela
+        # Start each new alien near the top left of the screen.
         self.rect.x = self.rect.width
         self.rect.y = self.rect.height
 
-        # Armazena a posição exata do alienígena
+        # Store the alien's exact position.
         self.x = float(self.rect.x)
 
-    def blitme(self):
-        """Desenha o alien em sua posição inicial"""
-        self.screen.blit(self.image, self.rect)
-
     def check_edges(self):
-        """Devolve True se o alienígena estiver na borda da tela."""
+        """Return True if alien is at edge of screen."""
         screen_rect = self.screen.get_rect()
         if self.rect.right >= screen_rect.right:
             return True
@@ -34,7 +30,11 @@ class Alien(Sprite):
             return True
 
     def update(self):
-        """Move o alienígena para a direita ou para a esquerda."""
+        """Move the alien right or left."""
         self.x += (self.ai_settings.alien_speed_factor *
                    self.ai_settings.fleet_direction)
         self.rect.x = self.x
+
+    def blitme(self):
+        """Draw the alien at its current location."""
+        self.screen.blit(self.image, self.rect)
